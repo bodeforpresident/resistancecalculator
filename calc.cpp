09c2d91d188f89e371a9cd1ec42a46526e491747
Bode <289588@burlcoschools.org>
2026-04-23 09:27:05 -0400

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -4,42 +4,54 @@
 #include <numeric>
 using namespace std;
 
-void parEq(double& resistor, vector<int>& resistors, vector<int>& resistorsOver2);
+double parEq(double& resistor, vector<double>& resistors, vector<double>& resistorsOver2);
+double serEq(vector<double>& resistors);
 
 int main() {
     double resistor;
     int resistorAmt;
     int loopIt = 0;
     
-    vector<int> resistors;
-    vector<int> resistorsOver2;
+    vector<double> resistors;
+    vector<double> resistorsOver2;
     
+    cout << "how many resistors do you have?\n";
     cin >> resistorAmt;
     
     do {
+        cout << "enter R" << loopIt + 1 << "\n";
         cin >> resistor;
         resistors.push_back(resistor);
         loopIt++;
     } while (resistorAmt > loopIt);
     
+    cout << "\n";
+    
     double sRT = accumulate(resistors.begin(), resistors.end(), 0);
+    cout << "series resistance total: ";
     cout << sRT << "\n";
     
+    cout << "parallel resistance total: ";
     if (resistorAmt == 2) {
-        double pRT = ((resistors[0]) * (resistors[1]))/(resistors[0] + resistors[1]);
-        cout << pRT;
+        cout << serEq(resistors) << " ohms\n";
     } else if (resistorAmt > 2) {
-        int rec = 0;
-    
-        for (int i = 0; i < resistors.size(); i++) {
-            rec = (1 / resistors[i]);
-            resistorsOver2.push_back(rec);
-        }
-    
-        double total = (1 / accumulate(resistorsOver2.begin(), resistorsOver2.end(), 0));
-        cout << total << "\n";
+        cout << parEq(resistor, resistors, resistorsOver2) << " ohms\n";
     }
 }
 
-void parEq(double& resistor, vector<int>& resistors, vector<int>& resistorsOver2) {
+double parEq(double& resistor, vector<double>& resistors, vector<double>& resistorsOver2) {
+    double rec = 0.0;
+    int loop = 0;
+        
+    while (loop < resistors.size()) {
+        rec = rec + (1 / resistors[loop]);
+        loop++;
+    }
+    
+    double total = 1 / rec;
+    return total;  
+}
+
+double serEq(vector<double>& resistors) {
+    return ((resistors[0]) * (resistors[1]))/(resistors[0] + resistors[1]);
 }
